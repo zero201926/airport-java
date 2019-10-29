3d2af078f15b8accb57e37de17d4779c9b46aa77
vikash <zero_transfer@outlook.com>
2019-10-29 09:57:53 +0000

    done

--- spec/AirControlSpec.js
+++ spec/AirControlSpec.js
@@ -1,29 +1,29 @@
-describe("Airport", function() {
-
-  var airport;
-  var weather;
-  beforeEach(function() {
-  airport = new Airport(20, weather);
-  // weather = new Weather();
-  plane = jasmine.createSpyObj('plane', ["8457"]);
-  airport.plane_storage.push(plane);
-  weather = jasmine.createSpyObj('weather', ["condition": 'good']);
-});
-  it('instructs a plane to land at an airport.', function() {
-    airport.land(plane);
-    expect(airport.plane_storage).toContain(plane);
-  });
-    //weather = double(:weather, :condition => 'good')
-  it('intructs plane to take off from an airport.', function() {
-    airport.plane_storage.push(plane);
-    airport.take_off(plane);
-    expect(airport.plane_storage).not.toContain(plane);
-  });
-
-  it('confirm a plane that has taken off is no longer at the airport.', function() {
-    airport.take_off(plane);
-    expect(airport.plane_status(plane);).toEqual(false)
-  });
+// describe("Airport", function() {
+//   let airport = null
+//   let weather = null
+//   let plane = null
+//   beforeEach(function() {
+//   airport = new Airport(weather);
+//   // weather = new Weather();
+//   plane = jasmine.createSpyObj('plane', ["8457"]);
+//   airport.plane_storage.push(plane);
+//   weather = jasmine.createSpyObj('weather', ['good']);
+// });
+//   it('instructs a plane to land at an airport.', function() {
+//     airport.land(plane);
+//     expect(airport.plane_storage).toContain(plane);
+//   });
+//     //weather = double(:weather, :condition => 'good')
+//   it('intructs plane to take off from an airport.', function() {
+//     airport.plane_storage.push(plane);
+//     airport.take_off(plane);
+//     expect(airport.plane_storage).not.toContain(plane);
+//   });
+//
+//   it('confirm a plane that has taken off is no longer at the airport.', function() {
+//     airport.take_off(plane);
+//     expect(airport.plane_status(plane)).toEqual(false)
+//   });
   /*
   it('prevents the landing of a plane when the airport is full.', function() {
     airport.capacity.times { airport.plane_storage << plane }
@@ -32,16 +32,16 @@ describe("Airport", function() {
     expect { airport.land(plane) }.to raise_error("Airport is at max capacity!!!ABORT!")
   end
 */
-  it('prevents takeoff when weather is stormy', function() {
-    weather = jasmine.createSpyObj("weather", ["condition": 'stormy']);
-    airport = new Airport(20, weather);
-    expect(function(){ airport.take_off(plane); }).toThrowError("BAD WEATHER, ALL PLANES GROUNDED");
-  });
-  it('prevents landing when weather is stormy', function() {
-    weather = jasmine.createSpyObj("weather", ["condition": 'stormy']);
-    airport = new Airport(20, weather);
-    expect(function(){ airport.land(plane); }).toThrowError("BAD WEATHER, NO LANDING");
-  });
+  // it('prevents takeoff when weather is stormy', function() {
+  //   weather = jasmine.createSpyObj("weather", ['stormy']);
+  //   airport = new Airport(weather);
+  //   expect(function(){ airport.take_off(plane); }).toThrowError("BAD WEATHER, ALL PLANES GROUNDED");
+  // });
+  // it('prevents landing when weather is stormy', function() {
+  //   weather = jasmine.createSpyObj("weather", ['stormy']);
+  //   airport = new Airport(weather);
+  //   expect(function(){ airport.land(plane); }).toThrowError("BAD WEATHER, NO LANDING");
+  // });
   /*
   it('default airport capacity that can be overridden as appropriate', function() {
     airport = new Airport(50, weather);
@@ -51,4 +51,48 @@ describe("Airport", function() {
   end
 end
 */
+// });
+'use strict';
+
+describe('Airport', function(){
+  var airport;
+  var plane;
+  var weather;
+
+  beforeEach(function(){
+    plane = jasmine.createSpy('plane');
+    weather = jasmine.createSpyObj('weather', ['isStormy']);
+    airport = new Airport(weather);
+  });
+
+  it('has no planes by default', function(){
+    expect(airport.planes()).toEqual([]);
+  });
+
+  describe('under normal conditions',function(){
+    beforeEach(function(){
+      weather.isStormy.and.returnValue(false);
+    });
+    it('can clear planes for landing', function(){
+      airport.clearForLanding(plane);
+      expect(airport.planes()).toEqual([plane]);
+    });
+    it('can clear planes for takeoff', function(){
+      airport.clearForLanding(plane);
+      airport.clearForTakeOff(plane);
+      expect(airport.planes()).toEqual([]);
+    });
+  });
+
+  describe('under stormy conditions',function(){
+    beforeEach(function(){
+      weather.isStormy.and.returnValue(true);
+    });
+    it('does not clear planes for landing', function(){
+      expect(function(){ airport.clearForLanding(plane); }).toThrowError('cannot land during storm');
+    });
+    it('does not clear planes for takeoff', function(){
+      expect(function(){ airport.clearForTakeOff(plane); }).toThrowError('cannot takeoff during storm');
+    });
+  });
 });
--- src/AirControl.js
+++ src/AirControl.js
@@ -1,29 +1,62 @@
-function Airport() {
-  Airport.prototype.initialize = function(weather = new Weather){
-    var weather = weather
-    let plane_storage = []
-  };
-  Airport.prototype.take_off = function(plane_id){
-    if(stormy? == true){
-      throw new Error('BAD WEATHER, ALL PLANES GROUNDED');
-    }
-    plane_storage = []
-  }
-  Airport.prototype.land = function(plane_id){
-    if(stormy? == true){
-      throw new Error('BAD WEATHER, NO LANDING');
-    }
-    plane_storage.push(plane_id)
-  }
-  Airport.prototype.plane_status = function(plane_id){
-    plane_storage.includes(plane_id);
-  }
-  Airport.prototype.stormy? = function(){
-    weather.condition == 'stormy';
-  }
+// function Airport() {
+//   Airport.prototype.initialize = function(weather = new Weather){
+//     var weather = weather
+//     let plane_storage = []
+//   };
+//   Airport.prototype.take_off = function(plane_id){
+//     if(stormy()){
+//       throw new Error('BAD WEATHER, ALL PLANES GROUNDED');
+//     }
+//     plane_storage = []
+//   }
+//   Airport.prototype.land = function(plane_id){
+//     if(stormy()){
+//       throw new Error('BAD WEATHER, NO LANDING');
+//     }
+//     plane_storage.push(plane_id)
+//   }
+//   Airport.prototype.plane_status = function(plane_id){
+//     plane_storage.includes(plane_id);
+//   }
+//   Airport.prototype.stormy = function(){
+//     weather == 'stormy';
+//   }
+// }
+'use strict';
+
+function Airport(weather){
+  this._weather = typeof weather !== 'undefined' ? weather : new Weather();
+  this._hangar = [];
+//   this.planes = function(){
+//     return hanger;
+//   }
+//   this.clearForLanding = function(plane) {
+//     if(this.isStormy()) {
+//       throw new Error('cannot land during storm');
+//     }
+//     hanger.push(plane);
+//   };
+//   this.clearForTakeOff = function(plane){
+//     if(this.isStormy()) {
+//       throw new Error('cannot takeoff during storm');
+//     }
+//     hanger = [];
+//   };
+//   this.isStormy = function(){
+//     return false;
+//   };
 }
 
-
-Player.prototype.makeFavorite = function() {
-  this.currentlyPlayingSong.persistFavoriteStatus(true);
+Airport.prototype.planes = function(){ return this._hangar; };
+Airport.prototype.clearForLanding = function(plane) {
+  if(this._weather.isStormy()) {
+    throw new Error('cannot land during storm');
+  }
+  this._hangar.push(plane);
+};
+Airport.prototype.clearForTakeOff = function(plane) {
+  if(this._weather.isStormy()) {
+    throw new Error('cannot takeoff during storm');
+  }
+  this._hangar = [];
 };
